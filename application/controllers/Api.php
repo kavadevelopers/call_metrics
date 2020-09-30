27c0a2a1d757491956f23e9a98e1a2c7d1f32164
Kava Developers <mehul9921@gmail.com>
2020-09-30 12:11:49 +0530

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -664,7 +664,7 @@ class Api extends CI_Controller
 				'goal_calls'		=> $this->getGoal($_user['goal_calls'],$calls,20),
 				'goal_conver'		=> $this->getGoal($_user['goal_conversation'],$conversations,20),
 				'goal_minute'		=> $this->getGoal($_user['goal_minutes'],$seconds,20,true),
-				'goal_avg_call'		=> $this->getAvgCallDec($conversations,$seconds)
+				'goal_avg_call'		=> $this->getGoal($_user['goal_avg_call'],$this->getAvgCallDec($conversations,$seconds),20)
 			];
 		}else if($type == "date"){
 			$this->db->where('date >=',$date);
@@ -740,7 +740,7 @@ class Api extends CI_Controller
 				'goal_calls'		=> $this->getGoal($_user['goal_calls'],$calls,1),
 				'goal_conver'		=> $this->getGoal($_user['goal_conversation'],$conversations,1),
 				'goal_minute'		=> $this->getGoal($_user['goal_minutes'],$seconds,1,true),
-				'goal_avg_call'		=> $this->getGoal($_user['goal_avg_call'],$this->getAvgCallDec($conversations,$seconds),1)
+				'goal_avg_call'		=> $this->getAvgCallDec($conversations,$seconds)
 			];
 		}else if($type == "week"){
 			$this->db->where('date >=',$this->x_week_range(date('Y-m-d'))[0]);
